a54081a97e3409b66c669450255fcf2e2c7d420f
Kirsten F <kfanker@gmail.com>
2025-06-04 15:52:55 -0500

    Create themeConfig.js

---
+++ js/themeConfig.js
@@ -0,0 +1,29 @@
+const customThemeColors = {
+    light: {
+        colors: {
+            primary: '#013886',
+            secondary: '#424242',
+            accent: '#FFC107',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107',
+
+            customBrandBlue: '#0A2E59',
+            customBrandGreen: '#4CAF50',
+            customBrandAccent: '#FFD700',
+            customNeutralLight: '#F5F5F5',
+            customNeutralDark: '#333333',
+        },
+    },
+    dark: {
+      colors: {
+      primary: '#BB86FC',
+      secondary: '#B0BEC5',
+      surface: '#121212',
+      background: '#121212',
+      }
+    }
+};
+
+export { customThemeColors };
